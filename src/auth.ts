6064f3a07d2d490bbb70d5d3ea0adc8b9a9e045b
AUM <omjha50@gmail.com>
2025-10-03 17:31:58 +0530

    linkAccount method implemented

--- src/auth.ts
+++ src/auth.ts
@@ -23,12 +23,23 @@ declare module "next-auth/jwt" {
 }
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+    events: {
+        async linkAccount({ user }) {
+            await db.user.update({
+                where: {
+                    id: user.id
+                }, data: {
+                    emailVerified: new Date(),
+                }
+            })
+        }
+    },
     callbacks: {
         async session({ token, session }) {
             if (token.sub && session.user) {
                 session.user.id = token.sub;
             }
-            console.log("sessionToken:", token);
+
             if (token.role && session.user) {
                 session.user.role = token.role;
             }
